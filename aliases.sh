6f80075f5334e3f76b01fdc63636ccbecfb0dc19
Art Gillespie <art@trainify.io>
2020-02-25 17:30:05 -0800

    add pgl alias

--- aliases.sh
+++ aliases.sh
@@ -1,3 +1,13 @@
+# ngrok-related aliases
+
+# get the first ngrok url
 alias ngurl="curl -s http://localhost:4040/api/tunnels | jq -r '.tunnels[0].public_url'"
+
+# get the first ngrok url and copy it to the clipboard
 alias ngcp="ngurl | xclip -sel clip"
+
+# postgresql-related aliases
+
+alias pgl="psql --host=localhost"
+
 
